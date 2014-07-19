b4d985ea4d44918101d9a12fa6dab83b79411634
staddaleko <staddaleko@gmail.com>
2014-07-19 14:34:45 +0200

    test

---
+++ S1/Controllers/KobietaController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace S1.Models
+{
+    public class KobietaController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            Auta nr1 = new Auta();
+            nr1.Marka = "Bentley";
+            nr1.Model = "Continental";
+            nr1.Nadwozie = "Coupe Sport";
+            nr1.Paliwo = "benzyna";
+            nr1.Pojemnosc_Silnika = 5998;
+            nr1.Rok_Produkcji = 2005;
+            nr1.Cena = 479000;
+
+            Auta nr2 = new Auta();
+            nr2.Cena = 20000;
+            nr2.Marka = "Bentley";
+            nr2.Model = "Turbo R";
+            nr2.Nadwozie = "sedan";
+            nr2.Paliwo = "benzyna";
+            nr2.Pojemnosc_Silnika = 6753;
+            nr2.Rok_Produkcji = 1985;
+
+            Auta nr3 = new Auta();
+            nr3.Cena = 477009;
+            nr3.Marka = "Aston Martin";
+            nr3.Model = "V12 Vanquish";
+            nr3.Nadwozie = "coupe";
+            nr3.Paliwo = "diesel";
+            nr3.Pojemnosc_Silnika = 6000;
+            nr3.Rok_Produkcji = 2013;
+
+            List<Auta> lista = new List<Auta> { nr1, nr2, nr3 };
+
+            ViewBag.AutoNr1 = JsonHelper.JsonSerializer<Auta>(nr1);//można by z tego utworzyć deserializowaną listę.
+            ViewBag.AutoNr2 = JsonHelper.JsonSerializer<Auta>(nr2);
+            ViewBag.AutoNr3 = JsonHelper.JsonSerializer<Auta>(nr3);
+
+
+            ViewBag.Serializacja = JsonHelper.JsonSerializer<List<Auta>>(lista);
+            List<Auta> Deserializacja = JsonHelper.JsonDeserialize<List<Auta>>(ViewBag.Serializacja);
+
+
+            return View("Index", Deserializacja);
+        }
+    }
+}
---
+++ S1/Models/Json.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Text;
+using System.Runtime.Serialization.Json;
+
+
+namespace S1.Models
+{
+    public class Auta
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string Nadwozie { get; set; }
+        public int Rok_Produkcji { get; set; }
+        public int Pojemnosc_Silnika { get; set; }
+        public string Paliwo { get; set; }
+        public int Cena { get; set; }
+    }
+
+    public class JsonHelper
+    {
+
+        public static string JsonSerializer<T>(T t)
+        {
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(T));
+            MemoryStream ms = new MemoryStream();
+            ser.WriteObject(ms, t);
+            string jsonString = Encoding.UTF8.GetString(ms.ToArray());
+            ms.Close();
+            return jsonString;
+        }
+
+        public static T JsonDeserialize<T>(string jsonString)
+        {
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(T));
+            MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
+            T obj = (T)ser.ReadObject(ms);
+            return obj;
+        }
+    }
+}
